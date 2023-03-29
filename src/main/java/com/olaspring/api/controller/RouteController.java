57f03bccd0ab330431e241017bc991569a85c734
Tukkos <jpfcastro@id.uff.br>
2023-03-29 13:27:09 -0300

    First commit

---
+++ src/main/java/com/olaspring/api/controller/RouteController.java
@@ -0,0 +1,17 @@
+package com.olaspring.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class RouteController {
+
+    @GetMapping("/")
+    
+    public String Hello() {
+        return "Olá Spring!";
+    }
+}
